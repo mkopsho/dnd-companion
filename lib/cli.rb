f2533dff7ab44fd1a4578c7c439f5f2152804949
mkopsho <michael.kopsho@gmail.com>
2020-05-26 10:52:23 -0400

    Same #urls, #create_all, and menu work for equipment

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,16 +10,16 @@ class CLI
     puts
     puts "What would you like to learn about?".colorize(:light_green)
     puts
-    puts "1. 𝕤𝕡𝕖𝕝𝕝𝕤 | 2. 𝕔𝕠𝕟𝕕𝕚𝕥𝕚𝕠𝕟𝕤 | 3. 𝕖𝕢𝕦𝕚𝕡𝕞𝕖𝕟𝕥 | 4. 𝕖𝕩𝕚𝕥".colorize(:blue)
+    puts "1. 𝕤𝕡𝕖𝕝𝕝𝕤 | 2. 𝕖𝕢𝕦𝕚𝕡𝕞𝕖𝕟𝕥 | 3. 𝕔𝕠𝕟𝕕𝕚𝕥𝕚𝕠𝕟𝕤 | 4. 𝕖𝕩𝕚𝕥".colorize(:blue)
     puts
     user_input = gets.chomp.strip
     case user_input 
     when "1"
       self.spells_menu
     when "2"
-      self.conditions_menu
-    when "3"
       self.equipment_menu
+    when "3"
+      self.conditions_menu
     when "4"
       puts "Farewell, traveler!".colorize(:light_green)
     else
@@ -61,9 +61,53 @@ class CLI
           Description: #{spell.description}"
         end
       end
-      puts end_menu
+      puts
+      end_menu
     else
+      puts
+      end_menu
+    end
+  end
+  
+  def equipment_menu
+    equipment = Equipment.get_all("http://www.dnd5eapi.co/api/equipment/")
+    puts "A fine choice, traveler. I know about #{equipment.length} equipable items.".colorize(:light_green)
+    puts
+    Equipment.create_all("http://www.dnd5eapi.co/api/equipment/") unless Equipment.all.length > 0
+    puts "Would you rather:".colorize(:light_green)
+    puts
+    puts "1. Hear about them all? | 2. Search by name?".colorize(:light_green)
+    puts
+    user_input = gets.chomp.strip
+    if user_input == "1"
+      Equipment.all.each do |item|
+        puts "- #{item.name}".colorize(:blue)
+      end
       puts end_menu
+    elsif user_input == "2"
+      puts "Pragmatic! Please provide the name of the item and I will tell you all I know:".colorize(:light_green)
+      user_input = gets.chomp.strip.split(" ")
+      user_input = user_input.collect { |word| word.capitalize }
+      user_input = user_input.join(" ")
+      Equipment.all.each do |item|
+        if user_input == item.name
+          puts "Ah, the #{item.name} is one of my favorite items! A fine choice, traveler.\nHere's everything I know about #{item.name}:\n".colorize(:light_green)
+          # Make this nicer to look at. Like a page out of the PHB...
+          puts "
+          Category: #{item.equipment_category}
+          Category Range: #{item.category_range}
+          Cost: #{item.cost}
+          Damage: #{item.damage}
+          Weight: #{item.weight}
+          Range: #{item.range}
+          Properties: #{item.props}"
+        end
+      end
+      puts
+      end_menu
+    else
+      puts
+      end_menu
     end
   end
 
@@ -86,31 +130,6 @@ class CLI
     #puts "I don't know about that condition. Want to have another go?".colorize(:light_blue)
     puts
     end_menu
-  end
-
-  def equipment_menu
-    equipment = Equipment.get_all("http://www.dnd5eapi.co/api/equipment/")
-    puts "A fine choice, traveler. I know about #{equipment.length} equipable items.".colorize(:light_green)
-    puts 
-    puts "Would you rather:".colorize(:light_green)
-    puts
-    puts "1. Hear about them all? | 2. Search by name?".colorize(:blue)
-    puts
-    user_input = gets.chomp.strip
-    if user_input == "1"
-      equipment.each_with_index do |equipment, index|
-        puts "#{index + 1}. #{equipment}".colorize(:blue)
-      end
-      #user input
-      #info method to go here
-    else
-      puts "Pragmatic! Please provide the name of the item and I will tell you all I know:".colorize(:light_green)
-      #search method to go here
-      #user input
-      #info method to go here
-      puts
-      end_menu
-    end
   end
 
   def end_menu
--- lib/equipment.rb
+++ lib/equipment.rb
@@ -1,9 +1,58 @@
 class Equipment
+  attr_accessor :name, :equipment_category, :category_range, :cost, :damage, :range, :weight, :props
 
   @@all = []
 
-  def initialize
+  def initialize(name = nil, equipment_category = nil, category_range = nil, cost = nil, damage = nil, range = nil, weight = nil, props = nil)
+    @name = name
+    @equipment_category = equipment_category
+    @category_range = category_range
+    @cost = cost
+    @damage = damage
+    @range = range
+    @weight = weight
+    @props = props
     @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def self.urls(url)
+    equipment_urls = []
+    equipment = API.new(url).parse_json
+    equipment_array = equipment["results"]
+    equipment_array.each do |hash|
+      equipment_urls << url + hash["index"].downcase
+    end
+    equipment_urls
+  end
+
+  def self.create_all(url)
+    equipment_tableau = []
+    equipment = urls(url)
+    equipment.each do |equipment|
+      equipment_tableau << API.new(equipment).parse_json
+    end
+    equipment_tableau.each do |tableau|
+      name = tableau["name"]
+      equipment_category = tableau["equipment_category"]
+      category_range = tableau["category_range"] 
+      cost = tableau["cost"]["quantity"].to_s + tableau["cost"]["unit"]
+      damage = tableau["damage"]
+      #["damage_dice"] + " " + tableau["damage"]["damage_type"]["name"]
+      range = tableau["range"]
+      #["normal"] + " / " + tableau["range"]["long"]
+      weight = tableau["weight"]
+      props = tableau["properties"]
+      #[0]["name"] + " / " + tableau["properties"][1]["name"]
+      Equipment.new(name, equipment_category, category_range, cost, damage, range, weight, props)
+    end
   end
 
   def self.get_all(url)
